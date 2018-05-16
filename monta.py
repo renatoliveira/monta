d996686ef76eb909461bcb1c17bad34896279b5d
Renato Oliveira <ren811@gmail.com>
2018-05-16 08:53:14 -0300

    Remove command to mount using 'mount', which requires 'sudo'.

--- monta.py
+++ monta.py
@@ -11,17 +11,6 @@ def get_drive_by_name(drive_name):
 			if partition['label'] == argv[1]:
 				return partition['name']
 
-# first arg is 'monta.py', the second one is the drive name and the third arg is the destination path
-def main():
-	if len(argv) != 3:
-		print('Incorrect number of args in "argv".')
-		exit(1)
-	print(argv)
-	mount_result = check_output('mount {0} {1}'.format(get_drive_by_name(
-		argv[1]),
-		argv[0]
-	).split(' '))
-
 if __name__ == '__main__':
-	main()
+	print(get_drive_by_name(argv[1]))
 
