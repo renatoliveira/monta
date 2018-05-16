7ee94af2f9b9bd250889d6454603f605a7c83f6e
Renato Oliveira <ren811@gmail.com>
2018-05-16 19:40:21 -0300

    Unify application into single file. Added setup script and directories to gitignore.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+	name='monta',
+	version='1.0',
+	description='Disk mounting shortcut for use with dmenu.',
+	author='Renato Oliveira',
+	url='https://github.com/renatoliveira/monta',
+	include_package_data=True,
+	package_data={
+		'': [
+			'monta',
+			'license.txt'
+		]
+	},
+	scripts=[
+		'monta/scripts/monta',
+		'monta/scripts/desmonta',
+		'monta/monta-utils'
+	]
+)
+
